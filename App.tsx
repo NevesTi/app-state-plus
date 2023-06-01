70d6ff434ef620c819451514f1ad2ad68d1b28c1
NevesTi <ceelso.7@gmail.com>
2023-05-31 21:45:11 -0300

    subindo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
 
 export default function App() {
+
+  const [fonteCarreda, setFonteCarregada] = useState(false);
+
+  async function loadFonts() {
+    await Font.loadAsync({
+      mukta_bold: require('./assets/Mukta/Mukta-Bold.ttf'),
+      mukta_extra: require('./assets/Mukta/Mukta-ExtraBold.ttf'),
+      mukta_regular: require('./assets/Mukta/Mukta-Regular.ttf'),
+
+
+    });
+  }
+
+  useEffect(() => {
+    loadFonts()
+      .then(() => {
+        setFonteCarregada(true);
+
+      })
+      .catch((error) => {
+        console.log('oorreu un erro', error);
+      })
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {fonteCarreda ?
+        <Text style={{ fontFamily: 'mukta_bold', fontSize: 50 }}>Aula de fontes!</Text>
+        :
+        <Text>Aula de fontes!</Text>
+
+      }
+
       <StatusBar style="auto" />
     </View>
   );
